03437059ab491302314d20a9c646d8f64868e082
Marko Heijnen <info@markoheijnen.com>
2015-12-26 13:25:54 +0100

    Update example template to latest code base.

--- docs/example-templates/front-page.php
+++ docs/example-templates/front-page.php
@@ -1,18 +1,19 @@
 <?php get_header(); ?>
 
+	<?php if ( class_exists( 'WP_Central_Stats' ) ) { ?>
 	<div id="content" class="container">
 
-		<h1 id="site-logo"><span>WordPress</span> <?php echo WP_Central_Stats::latest_version(); ?> stats</h1>
+		<h1 id="site-logo"><span>WordPress</span> <?php echo WP_Central_Stats::wp_version(); ?> stats</h1>
 		<h2 id="download-counter"><?php echo WP_Central_Stats::wordpress_downloads(); ?> downloads</h2>
 
 		<?php
-			echo wordpress_stats_graph( 'morris', 'line_chart', WP_Central_Stats::downloads_per_day(), array( 'x' => 'date', 'y' => 'count', 'label' => 'Downloads' ) );
+			echo WP_Central_Graph::get( 'morris', 'line_chart', WP_Central_Stats::downloads_per_day(), array( 'x' => 'date', 'y' => 'count', 'label' => 'Downloads' ) );
 		?>
 
 		<div class="row">
 			<div class="col-md-6">
 				<h2 class="text-center">Last 7 days</h2>
-				<?php echo wordpress_stats_graph( 'morris', 'bar', WP_Central_Stats::downloads_last7days(), array( 'x' => 'label', 'y' => 'value', 'label' => 'Downloads' ) ); ?>
+				<?php echo WP_Central_Graph::get( 'morris', 'bar', WP_Central_Stats::downloads_last7days(), array( 'x' => 'label', 'y' => 'value', 'label' => 'Downloads' ) ); ?>
 			</div>
 
 			<div class="col-md-6">
@@ -23,8 +24,8 @@
 					$releases = WP_Central_Stats::get_minor_releases();
 					foreach( $releases as $release ) {
 						echo "\t\t\t\t";
-						echo '<a href="' . $release['link'] . '" class="list-group-item">';
-						echo $release['title'];
+						echo '<a href="' . $release->link . '" class="list-group-item">';
+						echo $release->title;
 						echo '</a>';
 					}
 					?>
@@ -36,12 +37,12 @@
 		<div class="row">
 			<div class="col-md-6">
 				<h2 class="text-center">Downloads per day</h2>
-				<?php echo wordpress_stats_graph( 'chartjs', 'radar_chart', WP_Central_Stats::counts_per_day() ); ?>
+				<?php echo WP_Central_Graph::get( 'chartjs', 'radar_chart', WP_Central_Stats::counts_per_day() ); ?>
 			</div>
 
 			<div class="col-md-6">
 				<h2 class="text-center">Downloads per hour</h2>
-				<?php echo wordpress_stats_graph( 'chartjs', 'line_chart', WP_Central_Stats::counts_per_hour(), array(
+				<?php echo WP_Central_Graph::get( 'chartjs', 'line_chart', WP_Central_Stats::counts_per_hour(), array(
 					'options' => array(
 						'pointHitDetectionRadius' => 5,
 					),
@@ -51,6 +52,7 @@
 		</div>
 
 	</div>
+	<?php } ?>
 
 
 <?php get_footer(); ?>
