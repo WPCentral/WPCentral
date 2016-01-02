fc34fdfeacbfd0ae9121a4b679239356465ee9ee
Marko Heijnen <info@markoheijnen.com>
2016-01-02 23:50:35 +0100

    Set 404 when version doesn't exist.

--- inc/versions.php
+++ inc/versions.php
@@ -8,6 +8,7 @@ class WP_Central_Versions {
 	public function __construct() {
 		add_action( 'init', array( $this, 'add_rewrite_rule' ) );
 		add_filter( 'query_vars', array( $this, 'query_vars' ) );
+		add_action( 'wp', array( $this, 'set_404' ) );
 	}
 
 	public function add_rewrite_rule() {
@@ -18,6 +19,20 @@ class WP_Central_Versions {
 		$query_vars[] = 'wp_version_selector';
 
 		return $query_vars;
+	}
+
+	public function set_404( $wp ) {
+		global $wp_query;
+
+		if ( isset( $wp->query_vars['wp_version_selector'] ) ) {
+			$releases = WP_Central_Stats::get_major_releases();
+
+			if ( ! in_array( $wp->query_vars['wp_version_selector'], $releases ) ) {
+				$wp_query->set_404();
+				status_header( 404 );
+				nocache_headers();
+			}
+		}
 	}
 
 }
